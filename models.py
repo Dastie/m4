32b085ac4f0c3398b3c828ca23fb47f242b00de8
Dast <136837180+Dastie@users.noreply.github.com>
2023-08-07 21:22:55 +0500

    Панель администрирования

--- models.py
+++ models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils import timezone
 
 class Advertisement(models.Model):
     title = models.CharField(verbose_name='Название', max_length=128)
@@ -7,7 +10,27 @@ class Advertisement(models.Model):
     auction = models.BooleanField('Торг', help_text='Укажите, если возможен торг')
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now_add=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.create_at.date() == timezone.now().date():
+            created_time = self.create_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.create_at.strftime('%d.%m.%Y')
+
+    @admin.display(description='Дата последнего обновления')
+    def updated_date(self):
+        if self.update_at.date() == timezone.now().date():
+            update_time = self.update_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: blue; font-weight: bold;">Сегодня в {}</span>', update_time
+            )
+        return self.update_at.strftime('%d.%m.%Y')
+    
     class Meta:
         db_table = 'advertisements'
+
     def __str__(self): 
         return f"Advertisement(id = {self.id}, title = {self.title}, price = {self.price})"
