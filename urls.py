3cf4e31c11c2248c3abbb92cd497ac7118b6b56c
Dast <136837180+Dastie@users.noreply.github.com>
2023-08-28 21:20:09 +0500

    Аутентификация

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-from .views import index, top_sellers
+from .views import profile_view, login_view, logout_view, register_view
 
 urlpatterns = [
-    path('', index, name='main-page'),
-    path('top-sellers/', top_sellers, name='top_sellers'),
+    path('profile/', profile_view, name='profile'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register')
 ]
--- views.py
+++ views.py
@@ -1,26 +1,42 @@
-from django.shortcuts import render, redirect, reverse
-from django.http import HttpResponse
-from .models import Advertisement
-from .forms import AdvertisementForm
+from django.shortcuts import render, redirect, reverse 
+from django.contrib.auth import authenticate, login, logout, forms
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
-def index(request):
-    advertisements = Advertisement.objects.all()
-    context = {'advertisements': advertisements}
-    return render(request, 'index.html', context)
+@login_required(login_url=reverse_lazy('login'))
+def profile_view(request):
+    return render(request, 'app_auth/profile.html')
 
-def top_sellers(request):
-    return render(request, 'top-sellers.html') 
+def login_view(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect(reverse('profile'))
+        return render(request, 'app_auth/login.html')
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect(reverse('profile'))
+    return render(request, 'app_auth/login.html', {'error': 'Пользователь не найден'})
 
-def advertisement_post(request):
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('login'))
+
+def register_view(request):
     if request.method == 'POST':
-        form = AdvertisementForm(request.POST, request.FILES)
+        form = forms.UserCreationForm(request.POST)
         if form.is_valid():
-            advertisement = Advertisement(**form.cleaned_data)
-            advertisement.user = request.user
-            advertisement.save()
-            return redirect(reverse('main-page'))
+            form.save()
+            username = form.cleaned_data.get('username') 
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect(reverse('profile'))
         else:
-            return render(request, 'advertisement-post.html', {'form': form})
-    form = AdvertisementForm()
-    context = {'form': form}
-    return render(request, 'advertisement-post.html', context)
+            form = forms.UserCreationForm()
+            return render(request, 'app_auth/register.html', {'form': form})
+        
+    form = forms.UserCreationForm()
+    return render(request, 'app_auth/register.html', {'form': form})
